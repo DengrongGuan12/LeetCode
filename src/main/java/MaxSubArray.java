900bc95d123409e9bb92e41ba13cd9f141181502
初实 <dengrong.gdr@cainiao.com>
2019-07-15 17:05:04 +0800

    update

--- src/main/java/MaxSubArray.java
+++ src/main/java/MaxSubArray.java
@@ -4,10 +4,15 @@ public class MaxSubArray {
         int tmp = nums[0];
         int max = nums[0];
         for (int i = 1;i<nums.length;i++){
-            if(tmp + nums[i] <= 0 || (tmp < 0 && nums[i] > 0)){
-                tmp = nums[i];
-            }else{
+//            if(tmp + nums[i] <= 0 || (tmp < 0 && nums[i] > 0)){
+//                tmp = nums[i];
+//            }else{
+//                tmp += nums[i];
+//            }
+            if (tmp > 0){
                 tmp += nums[i];
+            }else{
+                tmp = nums[i];
             }
             if(tmp > max){
                 max = tmp;
@@ -29,10 +34,10 @@ public class MaxSubArray {
 
     public static void main(String[] args) {
         int[] nums = new int[]{
-                -2,1,-3,4,-1,2,1,-5,4
+//                -2,1,-3,4,-1,2,1,-5,4
 //                -2,-3,-1, 1, -5
 //                -1,1,2,1
-//                2,-4,5
+                2,-4,5
         };
         MaxSubArray maxSubArray = new MaxSubArray();
         System.out.println(maxSubArray.maxSubArray(nums));
