011dec6b149bb15aef2a6b56c5781e0ee4a0658a
dengrongguan <dengrongguan@192.168.0.104>
2019-08-01 00:23:51 +0800

    update

--- src/main/java/MinWindow.java
+++ src/main/java/MinWindow.java
@@ -15,40 +15,28 @@ public class MinWindow {
         }
         int minLen = Integer.MAX_VALUE;
         String res = "";
-        for (int i = 0;i < ids.size();i++){
-            int sum = sum(charCount);
-            for (int k = i; k <=  i + t.length() - 1; k++){
-                sum --;
-            }
-            for (int j = i + t.length(); j < ids.size() ; j++){
-                if (sum == 0){
-
-                    break;
-                }
-                sum--;
+        for (int k = 0; k < t.length() - 1; k++){
+            if (charCount[s.charAt(ids.get(k))] > 0){
+                charCount[s.charAt(ids.get(k))] --;
             }
         }
-        int l=0,h = 0;
+        for (int i = 0;i <= ids.size() - t.length();i++){
+            int j = i + t.length() - 1;
+            while (j < ids.size()){
+                if (charCount[s.charAt(ids.get(j))] > 0){
+                    charCount[s.charAt(ids.get(j))]--;
+                }
+                if (sum(charCount) == 0){
+                    if (minLen > (ids.get(j) - ids.get(i))){
+                        minLen = ids.get(j) - ids.get(i);
+                        res = s.substring(ids.get(i), ids.get(j)+1);
+                    }
+                    break;
+                }
+                j++;
 
-        while (h < s.length()){
-            while(l < s.length() && charCount[s.charAt(l)] == 0) l++;
-            if (l > (s.length() - t.length())) break;
-            charCount[s.charAt(l)]--;
-            h = l+1;
-            while(sum(charCount) > 0 && h < s.length()){
-                if (charCount[s.charAt(h)] > 0){
-                    charCount[s.charAt(h)] --;
-                }
-                h++;
             }
-            if (sum(charCount) == 0){
-                if (h - l < minLen){
-                    minLen = h - l;
-                    res = s.substring(l, h);
-                }
-                charCount[s.charAt(l)]++;
-                l++;
-            }
+            charCount[s.charAt(ids.get(i))] ++;
         }
         return res;
     }
