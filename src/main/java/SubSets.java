63e3608ec818a97ff6aca4c0cddfeb81e147317d
初实 <dengrong.gdr@cainiao.com>
2019-08-06 14:11:24 +0800

    update

--- src/main/java/SubSets.java
+++ src/main/java/SubSets.java
@@ -55,3 +55,50 @@ class SubSetsNoDup{
         subSets.subsetsWithDup(nums);
     }
 }
+class ExistOnBoard{
+    int[][] d = new int[][]{
+            {-1,0},
+            {1,0},
+            {0,-1},
+            {0,1}
+    };
+    public boolean exist(char[][] board, String word) {
+        int[][] visited = new int[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[0].length; j++){
+                if (backtracking(0, i,j, word, board, visited)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean backtracking(int cur, int x, int y, String word, char[][] board, int[][] visited){
+        if (cur == word.length()){
+            return true;
+        }
+        if (x < 0 || y < 0 || x >= board.length || y >= board[0].length  || visited[x][y] > 0 ||board[x][y] != word.charAt(cur)){
+            return false;
+        }
+        visited[x][y] = 1;
+        boolean res = false;
+        for (int i = 0 ;i < d.length;i++){
+            if (backtracking(cur +1, x + d[i][0], y + d[i][1], word, board, visited)){
+                return true;
+            }
+        }
+        visited[x][y] = 0;
+        return res;
+
+
+    }
+    public static void main(String[] args) {
+        ExistOnBoard existOnBoard = new ExistOnBoard();
+        char[][] board = new char[][]{
+                {'A','B','C','E'},
+                {'S','F','C','S'},
+                {'A','D','E','E'}
+        };
+        System.out.println(existOnBoard.exist(board, "SFCSECBAS"));
+    }
+}
